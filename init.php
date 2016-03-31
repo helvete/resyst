5df22973ae91fab2746d01ec0dbc3b2dbc73d6f4
elim garam <4ht@seznam.cz>
2016-03-31 20:33:54 +0200

    RES-unrelated: Fixed incorrect BASE_URL building in case of HTTPS

--- init.php
+++ init.php
@@ -1,5 +1,6 @@
 <?php
 define('APPLICATION_PATH', realpath(__DIR__));
 define('CONFIG_FILE_NAME', APPLICATION_PATH . '/configuration.data.php');
-define('BASE_URI', "{$_SERVER['REQUEST_SCHEME']}://{$_SERVER['HTTP_HOST']}". explode('?', $_SERVER['REQUEST_URI'])[0]);
+define('BASE_URI', (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http")
+	. "://{$_SERVER['HTTP_HOST']}". explode('?', $_SERVER['REQUEST_URI'])[0]);
 ?>
